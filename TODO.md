9ae1c7832adb9b1ff21fed2b3aed77c18d73622f
Daniel Kim <danielynskim@gmail.com>
2022-02-07 18:54:05 -0500

    Add list view to admin

--- TODO.md
+++ TODO.md
@@ -1,8 +1,4 @@
 BACKEND DONE TODAY!
-* Model plurals
-* Forms, views, mixins
-* User validation
-* Add in all fields + validation on fields (sold)
 * Button function
 * Custom urls/slugs
 * HTML + frontend TOMORROW!
--- app/admin.py
+++ app/admin.py
@@ -4,28 +4,28 @@ from .models import *
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in User._meta.get_fields()]
 
 @admin.register(Accessory)
 class AccessoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in Accessory._meta.get_fields()]
 
 @admin.register(Artisan)
 class ArtisanAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in Artisan._meta.get_fields()]
 
 @admin.register(Build)
 class BuildAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in Build._meta.get_fields()]
 
 @admin.register(Keyboard)
 class KeyboardAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in Keyboard._meta.get_fields()]
 
 @admin.register(Keycap)
 class KeycapAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in Keycap._meta.get_fields()]
 
 @admin.register(Switch)
 class SwitchAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in Switch._meta.get_fields()]
