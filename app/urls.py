0601bd6841a6028c7d321b1838a9b671ee762ccb
Daniel Kim <danielynskim@gmail.com>
2022-02-10 16:09:13 -0500

    Update and fix model update urls

--- app/urls.py
+++ app/urls.py
@@ -11,7 +11,7 @@ urlpatterns += [
     path('accessory/', AccessoryListView.as_view(), name='accessory'),
     path('accessory/<int:pk>', AccessoryDetailView.as_view(), name='accessory-detail'),
     path('accessory/create/', AccessoryCreateView.as_view(), name='accessory-create'),
-    path('accessory/<int:pk>/', AccessoryUpdateView.as_view(), name='accessory-update'),
+    path('accessory/<int:pk>/update/', AccessoryUpdateView.as_view(), name='accessory-update'),
     path('accessory/<int:pk>/delete/', AccessoryDeleteView.as_view(), name='accessory-delete'),
 ]
 
@@ -19,7 +19,7 @@ urlpatterns += [
     path('artisan/', ArtisanListView.as_view(), name='artisan'),
     path('artisan/<int:pk>', ArtisanDetailView.as_view(), name='artisan-detail'),
     path('artisan/create/', ArtisanCreateView.as_view(), name='artisan-create'),
-    path('artisan/<int:pk>/', ArtisanUpdateView.as_view(), name='artisan-update'),
+    path('artisan/<int:pk>/update/', ArtisanUpdateView.as_view(), name='artisan-update'),
     path('artisan/<int:pk>/delete/', ArtisanDeleteView.as_view(), name='artisan-delete'),
 ]
 
@@ -27,7 +27,7 @@ urlpatterns += [
     path('build/', BuildListView.as_view(), name='build'),
     path('build/<int:pk>', BuildDetailView.as_view(), name='build-detail'),
     path('build/create/', BuildCreateView.as_view(), name='build-create'),
-    path('build/<int:pk>/', BuildUpdateView.as_view(), name='build-update'),
+    path('build/<int:pk>/update/', BuildUpdateView.as_view(), name='build-update'),
     path('build/<int:pk>/delete/', BuildDeleteView.as_view(), name='build-delete'),
 ]
 
@@ -35,7 +35,7 @@ urlpatterns += [
     path('keycap/', KeycapListView.as_view(), name='keycap'),
     path('keycap/<int:pk>', KeycapDetailView.as_view(), name='keycap-detail'),
     path('keycap/create/', KeycapCreateView.as_view(), name='keycap-create'),
-    path('keycap/<int:pk>/', KeycapUpdateView.as_view(), name='keycap-update'),
+    path('keycap/<int:pk>/update/', KeycapUpdateView.as_view(), name='keycap-update'),
     path('keycap/<int:pk>/delete/', KeycapDeleteView.as_view(), name='keycap-delete'),
 ]
 
@@ -43,7 +43,7 @@ urlpatterns += [
     path('keyboard/', KeyboardListView.as_view(), name='keyboard'),
     path('keyboard/<int:pk>', KeyboardDetailView.as_view(), name='keyboard-detail'),
     path('keyboard/create/', KeyboardCreateView.as_view(), name='keyboard-create'),
-    path('keyboard/<int:pk>/', KeyboardUpdateView.as_view(), name='keyboard-update'),
+    path('keyboard/<int:pk>/update/', KeyboardUpdateView.as_view(), name='keyboard-update'),
     path('keyboard/<int:pk>/delete/', KeyboardDeleteView.as_view(), name='keyboard-delete'),
 ]
 
@@ -51,6 +51,6 @@ urlpatterns += [
     path('switch/', SwitchListView.as_view(), name='switch'),
     path('switch/<int:pk>', SwitchDetailView.as_view(), name='switch-detail'),
     path('switch/create/', SwitchCreateView.as_view(), name='switch-create'),
-    path('switch/<int:pk>/', SwitchUpdateView.as_view(), name='switch-update'),
+    path('switch/<int:pk>/update/', SwitchUpdateView.as_view(), name='switch-update'),
     path('switch/<int:pk>/delete/', SwitchDeleteView.as_view(), name='switch-delete'),
 ]
