dc85a07e2f8c7ef74e2425a9f7f1324351173f73
Daniel Kim <danielynskim@gmail.com>
2022-02-22 14:53:26 -0500

    Production changes

--- keyboards/settings.py
+++ keyboards/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(zp-30ummq=x8ywxtt2$@mc1l1__ok02r4yjb3fhosey*-7de!'
+#SECRET_KEY = 'django-insecure-(zp-30ummq=x8ywxtt2$@mc1l1__ok02r4yjb3fhosey*-7de!'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -141,19 +146,10 @@ LOGIN_REDIRECT_URL = '/app/user/'
 
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # TODO: change this
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {"class": "logging.StreamHandler"},
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["console"],
-            "level": "INFO",
-        },
-    }
-}
+CSRF_COOKIE_SECURE = True
 
+SESSION_COOKIE_SECURE = True
+
+SECURE_SSL_REDIRECT = True
