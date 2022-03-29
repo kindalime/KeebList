d92fce269021d3fcf43cd28b3e161ea2e816749d
Daniel Kim <kindalime0@gmail.com>
2022-03-29 11:12:01 -0400

    Make migrations

--- keyboards/settings.py
+++ keyboards/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-(zp-30ummq=x8ywxtt2$@mc1l1__ok02r4yjb3fhosey*-7de!'
-# SECRET_KEY = (os.environ.get("MY_SECRET", 'dev default value'))
+SECRET_KEY = (os.environ.get("MY_SECRET", 'dev default value'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
