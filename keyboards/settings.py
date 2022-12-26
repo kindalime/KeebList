b93514339d21d8f270bd5990813b8660bde035a8
Daniel Kim <kindalime0@gmail.com>
2022-12-26 12:13:13 -0500

    Comment out heroku locals

--- keyboards/settings.py
+++ keyboards/settings.py
@@ -187,8 +187,8 @@ LOGGING = {
     },
 }
 
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
